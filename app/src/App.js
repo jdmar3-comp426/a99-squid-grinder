bc210b0f09d571cd90a14b2a1efed29faba7977e
Ushno Chakraborty <ushnochakraborty@ushnos-mbp.wireless-1x.unc.edu>
2021-12-01 18:33:27 -0500

    adding bulma import

--- app/src/App.js
+++ app/src/App.js
@@ -1,23 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bulma/css/bulma.min.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section class="hero is-dark is-fullheight">
+            <div class="hero-body">
+                <div class="container has-text-centered">
+                    <h1 id="main-title" class="title has-text-info"><span id="main-title" class="title">You</span>Topia</h1>
+                    <p class="subtitle has-text-centered">They win or they lose. You choose!</p>
+                    <div class="content ">
+                        <div class="columns">
+                            <div class="column has-text-right">
+                                <button class="button is-red">
+                                    <a href="./pages/login.html">Login</a>
+                                </button>
+                            </div>
+                            <div class="column has-text-left">
+                                <button class="button is-red">
+                                    <a href="./pages/signup.html">Sign-Up</a>
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
     </div>
   );
 }
