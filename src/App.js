104d3d795886cdbb4ca358eb3348a7b24111980f
Ushno Chakraborty <ushnochakraborty@Ushnos-MBP.lan>
2021-12-02 22:00:13 -0500

    adding route to page

--- src/App.js
+++ src/App.js
@@ -3,15 +3,30 @@ import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
 import power_button from './power-button.jpeg';
 import './App.css';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Routes } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import { Route } from 'react-router-dom';  
 
 function App() {
+  const [isHome, setIsHome] = useState(true);
+
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={power_button} className="App-logo" alt="logo" />
-        <Counter />
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <Routes>
+            <Route>
+            </Route>
+            <Route path="/clicker">
+              <img src={power_button} className="App-logo" alt="logo" />
+              <Counter />
+            </Route>
+          </Routes>
+        </header>
+      </div>
+    </Router>
   );
 }
 
