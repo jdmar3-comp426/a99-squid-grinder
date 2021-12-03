0e27eb25b883613064cbff9df4928af0fb679416
Ushno Chakraborty <ushnochakraborty@Ushnos-MBP.lan>
2021-12-02 22:51:01 -0500

    fixing router

--- src/App.js
+++ src/App.js
@@ -1,36 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
-import power_button from './power-button.jpeg';
 import './App.css';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Routes } from 'react-router-dom';
-import { Link } from 'react-router-dom';
+import { Li } from 'react-router-dom';
 import { Route } from 'react-router-dom';  
 import Home from './Home';
 import Login from './Login';
 import SignUp from './SignUp';
 
+
 function App() {
+
+  const [home, setHome] = useState(true);
   
   return (
     <Router>
       <div className="App">
         <header className="App-header">
           <Routes>
-            <Route path="/">
-              <Home />
-            </Route>
-            <Route path="/Login">
-              <Login />
-            </Route>
-            <Route path="/SignUp">
-              <SignUp />
-            </Route>
-            <Route path="/clicker">
-              <img src={power_button} className="App-logo" alt="logo" />
-              <Counter />
-            </Route>
+            <Route path="/" element={<Home/>}></Route>
+            <Route path="/clicker" element={<Counter />}></Route>
           </Routes>
         </header>
       </div>
