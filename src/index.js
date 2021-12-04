a06522b60b778c7fbf7af1b038fc33c70d0b26d0
Ushno Chakraborty <ushnochakraborty@Ushnos-MBP.lan>
2021-12-03 21:49:23 -0500

    setting up firebase

--- src/index.js
+++ src/index.js
@@ -6,6 +6,24 @@ import { store } from './app/store';
 import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD3uDCYMz7M-sXhTl2NhPTL-VsZMMK3LpY",
+  authDomain: "comp426-5ba6c.firebaseapp.com",
+  projectId: "comp426-5ba6c",
+  storageBucket: "comp426-5ba6c.appspot.com",
+  messagingSenderId: "18328278022",
+  appId: "1:18328278022:web:7f0da4847fb06a85c51f87"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
